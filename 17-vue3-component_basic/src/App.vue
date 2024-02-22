698d1b2bfe2ee16b49ed92d1296702f9ef4f5abb
PINGPINGYEE <owb7994@nate.com>
2024-02-22 16:08:57 +0900

    Feat: Vue 04일차 추가 학습

---
+++ 17-vue3-component_basic/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>no.1</h1>
+  <MyBtn />
+  <h1>no.2</h1>
+  <MyBtn text="Banana"/>
+  <h1>no.3</h1>
+  <MyBtn>Banana</MyBtn>
+  <h1>no.4</h1>
+  <MyBtn color="royalblue"/>
+  <h1>no.5</h1>
+  <MyBtn :color="123"/> <!-- 다음과 같이 바인딩을 해야 숫자로 인식(설정해 둔 type 때문에) -> 어차피 에러 -->
+  <h1>no.6</h1>
+  <MyBtn color="#000"/>
+  <h1>no.7</h1>
+  <MyBtn :color="color"/>
+  <h1>no.8</h1>
+  <MyBtn large color="royalblue"/>
+  <h1>no.9</h1>
+  <MyBtn :color="color"><span style="color: red;">Banana</span></MyBtn>
+  <br>
+  <button>Banana</button>
+</template>
+
+<script>
+import MyBtn from './components/MyBtn.vue'
+
+export default {
+  components: {
+    MyBtn
+  },
+  data() {
+    return {
+      color: '#000'
+    }
+  }
+}
+</script>
+
+<style>
+  h1 {
+    font-size: 15px;
+  }
+</style>
---
+++ 17-vue3-component_basic/src/components/MyBtn.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="{ large : large }" :style="{ backgroundColor: color }" class="btn">{{text}}</div>
+  <div :class="{ large : large }" :style="{ backgroundColor: color }" class="btn"><slot>Apple</slot></div>
+</template>
+
+<script>
+export default {
+  props: { // 컴포넌트가 실행이 될 때 마치 속성처럼 받아내는 내용들을 받아줄 수 있는 내용을 정의해 주는 옵션 + 부모, 자식 간의 데이터 통신
+    color: {
+      type: String,
+      default: 'gray'
+    },
+    large: {
+      type: Boolean,
+      default: false
+    },
+    text: {
+      type: String,
+      default: 'Apple'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .btn {
+    display: inline-block;
+    margin: 4px;
+    padding: 6px 12px;
+    border-radius: 4px;
+    background-color: gray;
+    color: white;
+    cursor: pointer;
+  }
+
+  .btn.large {
+    font-size:20px;
+    padding: 10px 20px;
+  }
+</style>
